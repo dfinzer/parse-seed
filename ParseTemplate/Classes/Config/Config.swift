a3f2f0b897c1053f1c0b551c213d4ef2c753636a
Robert Maciej Pieta <rmpieta@Roberts-MacBook-Pro.local>
2015-04-22 18:30:03 -0500

    seed

---
+++ ParseTemplate/Classes/Config/Config.swift
@@ -0,0 +1,20 @@
+//
+//  Config.swift
+//  ParseTemplate
+//
+//  Created by Robert Maciej Pieta on 4/22/15.
+//  Copyright (c) 2015 Gigster. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class ConfigManager {
+    class func parseAppId() -> String {
+        return ""
+    }
+    
+    class func parseClientKey() -> String {
+        return ""
+    }
+}
